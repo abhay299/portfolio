06e47b76b266523ae4d348ca087aa3f591c6aff0
Abhay Gupta <abhaysg2000@gmail.com>
2023-07-06 05:15:13 +0530

    revamped home page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,25 +15,40 @@ const Navbar = () => {
 	};
 
 	return (
-		<div className='navbar'>
-			<div className='navbarItems'>
-				{
-					location.pathname === current.home
-						? <Link to={"/"} style={{ color: '#F1C376', 'textDecoration': 'underline' }}>Home</Link>
-						: <Link to={"/"}>Home</Link>
-				}
-				{
-					location.pathname === current.project || location.pathname === current.projectID
-						? <Link to={"/projects"} style={{ color: '#F1C376', 'textDecoration': 'underline' }}>Projects</Link>
-						: <Link to={"/projects"}>Projects</Link>
-				}
-				{
-					location.pathname === current.about
-						? <Link to={"/aboutMe"} style={{ color: '#F1C376', 'textDecoration': 'underline' }}>About Me</Link>
-						: <Link to={"/aboutMe"}>About Me</Link>
-				}
+		// <div className='navbar'>
+		// 	<div className='navbarItems'>
+		// 		{
+		// 			location.pathname === current.home
+		// 				? <Link to={"/"} style={{ color: '#F1C376', 'textDecoration': 'underline' }}>Home</Link>
+		// 				: <Link to={"/"}>Home</Link>
+		// 		}
+		// 		{
+		// 			location.pathname === current.project || location.pathname === current.projectID
+		// 				? <Link to={"/projects"} style={{ color: '#F1C376', 'textDecoration': 'underline' }}>Projects</Link>
+		// 				: <Link to={"/projects"}>Projects</Link>
+		// 		}
+		// 		{
+		// 			location.pathname === current.about
+		// 				? <Link to={"/aboutMe"} style={{ color: '#F1C376', 'textDecoration': 'underline' }}>About Me</Link>
+		// 				: <Link to={"/aboutMe"}>About Me</Link>
+		// 		}
+		// 	</div>
+		// </div>
+		<body>
+			{/* <div className='navbar'> */}
+			<input type='checkbox' id='active' />
+			<label htmlFor='active' className='menu-btn'><span></span></label>
+			<label htmlFor='active' className='close'></label>
+			<div className='wrapper'>
+				<ul>
+					<li><Link to={"/"}>Home</Link></li>
+					<li><Link to={"/projects"}>Work</Link></li>
+					<li><Link to={"/aboutMe"}>About</Link></li>
+					<li><Link to={"/feedback"}>Feedback</Link></li>
+				</ul>
 			</div>
-		</div>
+			{/* </div> */}
+		</body>
 	)
 };
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,56 +1,122 @@
 import React from 'react'
 import './home.css';
-import me2 from '../../assets/me2.jpg'
-import { GitHub, LinkedIn, EmailSharp } from '@mui/icons-material';
+import smHomePage from '../../assets/socialMediaFeed.png';
+// import me2 from '../../assets/me2.jpg'
+// import { GitHub, LinkedIn, EmailSharp } from '@mui/icons-material';
 
 const Home = () => {
 
 	return (
 		<div className='home'>
-			<div className='infoContainer'>
-				<img
-					src={me2}
-					alt=''
-				/>
-				<div className='info'>
-					<p>Hello, I am</p>
-					<h1 style={{ 'color': 'midnightblue' }}>Abhay Gupta</h1>
-					<p>Web developer | Fresh graduate | Passionate about creating functional websites | Eager to contribute and grow professionally.</p>
-					<span>Get to know me more through</span>
-					<div className='checkout'>
+			<div className='info'>
+				<p>Hey, I'm</p>
+				<h1>Abhay Gupta</h1>
+				Full Stack Web developer
+				<p><br />
+					Passionate about creating functional websites | Eager to contribute and grow professionally.</p>
+				{/* <span>Connect with me</span>
+				<div className='checkout'>
 
-						<a href='https://github.com/abhay299/'>
-							<GitHub fontSize='small' />
-						</a>
-						|
-						<a href='https://www.linkedin.com/in/abhay-gupta-88326b1a6/'>
-							<LinkedIn fontSize='medium' />
-						</a>
-						|
-						<a href='mailto:abhaysg2000@gmail.com'>
-							<EmailSharp fontSize='medium' />
-						</a>
+					<a href='https://github.com/abhay299/'>
+						<GitHub fontSize='small' />
+					</a>
+					|
+					<a href='https://www.linkedin.com/in/abhay-gupta-88326b1a6/'>
+						<LinkedIn fontSize='medium' />
+					</a>
+					|
+					<a href='mailto:abhaysg2000@gmail.com'>
+						<EmailSharp fontSize='medium' />
+					</a>
+				</div> */}
+			</div>
+			<div className='work'>
+				<div className='projectCardContainer'>
+					{/* <img src={smHomePage} alt='' /> */}
+					<div className='containerText'>
+						<h2>Card Heading</h2>
+						<p>
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+							Nesciunt exercitationem iste, voluptatum,
+						</p>
 					</div>
+					{/* <a href='#' className='button'>
+						Find out more
+						<span className='material-symbols-outlined'>arrow_right_alt</span>
+					</a> */}
+				</div>
+				<div className='projectCardContainer'>
+					{/* <img src={smHomePage} alt='' /> */}
+					<div className='containerText'>
+						<h2>Card Heading</h2>
+						<p>
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+							Nesciunt exercitationem iste, voluptatum,
+						</p>
+					</div>
+					{/* <a href='#' className='button'>
+						Find out more
+						<span className='material-symbols-outlined'>arrow_right_alt</span>
+					</a> */}
+				</div>
+				<div className='projectCardContainer'>
+					{/* <img src={smHomePage} alt='' /> */}
+					<div className='containerText'>
+						<h2>Card Heading</h2>
+						<p>
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+							Nesciunt exercitationem iste, voluptatum,
+						</p>
+					</div>
+					{/* <a href='#' className='button'>
+						Find out more
+						<span className='material-symbols-outlined'>arrow_right_alt</span>
+					</a> */}
+				</div>
+				<div className='projectCardContainer'>
+					{/* <img src={smHomePage} alt='' /> */}
+					<div className='containerText'>
+						<h2>Card Heading</h2>
+						<p>
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+							Nesciunt exercitationem iste, voluptatum,
+						</p>
+					</div>
+					{/* <a href='#' className='button'>
+						Find out more
+						<span className='material-symbols-outlined'>arrow_right_alt</span>
+					</a> */}
+				</div>
+				<div className='projectCardContainer'>
+					{/* <img src={smHomePage} alt='' /> */}
+					<div className='containerText'>
+						<h2>Card Heading</h2>
+						<p>
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+							Nesciunt exercitationem iste, voluptatum,
+						</p>
+					</div>
+					{/* <a href='#' className='button'>
+						Find out more
+						<span className='material-symbols-outlined'>arrow_right_alt</span>
+					</a> */}
 				</div>
 			</div>
-			<div className='skillContainer'>
-				<div className='skills'>
-					<h2 style={{ 'color': 'midnightblue' }}>Tech Skills</h2>
-					<ul>
-						<li>
-							Front-End: ReactJS, HTML, CSS
-						</li>
-					</ul>
-					<ul>
-						<li>
-							Back-End: NodeJS, ExpressJS, Django, MySQL
-						</li>
-					</ul>
-					<h2 style={{ 'color': 'midnightblue' }}>Languages</h2>
-					<ul>
-						<li>JavaScript, Python, Java, C</li>
-					</ul>
-				</div>
+			<div className='skill'>
+				<h2>Tech Skills</h2>
+				<ul className='skillInfo'>
+					<li>
+						Front-End: ReactJS, HTML, CSS
+					</li>
+					<br />
+					<li>
+						Back-End: NodeJS, ExpressJS, Django, MySQL
+					</li>
+				</ul>
+				<h2>Languages</h2>
+				<ul className='skillInfo'>
+					<li>JavaScript, Python, Java, C</li>
+				</ul>
 			</div>
 		</div>
 	)
