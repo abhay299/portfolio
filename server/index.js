d3ef2a851affcda4b505af74453080eb697af35a
Abhay Gupta <abhaysg2000@gmail.com>
2023-07-11 18:07:09 +0530

    server side update

--- server/index.js
+++ server/index.js
@@ -3,27 +3,35 @@ import cors from "cors";
 import db from "./connect.js";
 import moment from "moment/moment.js";
 import cookieParser from "cookie-parser";
-import jwt from 'jsonwebtoken';
+import Cookies from "universal-cookie";
 
 const app = express();
 // const router = express.Router();
-
+app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
-	res.header({
-		"Access-Control-Allow-Credentials": true,
-		"Access-Control-Allow-Private-Network": true,
-	});
+
+	const corsWhiteList = [
+		"http://localhost:3000",
+		"https://abhay-gupta.netlify.app",
+		"https://abhay-develop.netlify.app"
+	];
+
+	if (corsWhiteList.indexOf(req.headers.origin) !== -1) {
+		res.header("Access-Control-Allow-Origin", req.headers.origin)
+		res.header("Access-Control-Allow-Headers", 'Origin, X-Request-With, Content-Type, Accept');
+		res.header({
+			"Access-Control-Allow-Credentials": true,
+			"Access-Control-Allow-Private-Network": true,
+		});
+	}
+
 	next();
 });
 
-app.use(express.json());
-app.use(cors({
-	// origin: "http://localhost:3000"
-	origin: "https://abhay-gupta.netlify.app"
-}));
-
 app.use(cookieParser());
-
+const cookies = new Cookies();
+cookies.set({ secure: true, sameSite: 'none' });
 db.connect((err) => {
 	if (err) throw err;
 	console.log("Database Connected!")
@@ -32,28 +40,7 @@ db.connect((err) => {
 // Add new feedback
 app.post('/', (req, res) => {
 
-	// const token = req.cookies.accessToken;
-	// if (!token) return res.status(401).json("Something went wrong");
-
-	// jwt.verify(token, "secretkey", (err, data) => {
-	// 	if (err) return res.status(403).json("Token is not valid");
-
-	// 	const q = "INSERT INTO feedback (`name`, `email`, `comment`, `createdAt`) VALUES (?)";
-	// 	const values = [
-	// 		req.body.name,
-	// 		req.body.email,
-	// 		req.body.comment,
-	// 		moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
-	// 	];
-
-	// 	db.query(q, [values], (err, data) => {
-	// 		if (err) return res.status(500).json(err);
-	// 		return res.status(200).json("Message has been received.")
-	// 	});
-	// });
-	// console.log(req.body.name);
-	// console.log("What is Data: ", res);
-
+	console.log("req:", req.body)
 	const name = req.body.name;
 	const email = req.body.email;
 	const comment = req.body.comment;
