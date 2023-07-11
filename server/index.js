cbf587035bf78b4ae4f984e3a4f0ca9da40f6539
Dhruv Maddirala <dhruv.maddirala@gmail.com>
2023-07-11 23:30:25 +0530

    api added

--- server/index.js
+++ server/index.js
@@ -17,46 +17,65 @@ app.use((req, res, next) => {
 		"https://abhay-develop.netlify.app"
 	];
 
-	if (corsWhiteList.indexOf(req.headers.origin) !== -1) {
-		res.header("Access-Control-Allow-Origin", req.headers.origin)
-		res.header("Access-Control-Allow-Headers", 'Origin, X-Request-With, Content-Type, Accept');
-		res.header({
-			"Access-Control-Allow-Credentials": true,
-			"Access-Control-Allow-Private-Network": true,
-		});
-	}
+  if (corsWhiteList.indexOf(req.headers.origin) !== -1) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Request-With, Content-Type, Accept"
+    );
+    res.header({
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Private-Network": true,
+    });
+  }
 
-	next();
+  next();
 });
 
 app.use(cookieParser());
 const cookies = new Cookies();
-cookies.set({ secure: true, sameSite: 'none' });
+cookies.set({ secure: true, sameSite: "none" });
 db.connect((err) => {
-	if (err) throw err;
-	console.log("Database Connected!")
-})
+  if (err) throw err;
+  console.log("Database Connected!");
+});
 
 // Add new feedback
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
+  console.log("req:", req.body);
+  const name = req.body.name;
+  const email = req.body.email;
+  const comment = req.body.comment;
+  const dateTime = moment(Date.now()).format("YYYY-MM-DD HH:mm:ss");
 
-	console.log("req:", req.body)
-	const name = req.body.name;
-	const email = req.body.email;
-	const comment = req.body.comment;
-	const dateTime = moment(Date.now()).format("YYYY-MM-DD HH:mm:ss");
+  db.query(
+    "INSERT INTO feedback (name, email, comment, createdAt) VALUES (?, ?, ?, ?)",
+    [name, email, comment, dateTime],
+    (err, result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        res.send(err);
+      }
+    }
+  );
+});
 
-	db.query("INSERT INTO feedback (name, email, comment, createdAt) VALUES (?, ?, ?, ?)", [name, email, comment, dateTime],
-		(err, result) => {
-			if (result) {
-				res.send(result);
-			} else {
-				res.send(err);
-			}
-		}
-	);
+app.get("/feedback", function (req, res) {
+  db.query(
+    "SELECT * FROM feedback",
+    [],
+    (err, result) => {
+			console.log(result);
+      if (result) {
+        res.end(result)
+      } else {
+        res.end(err)
+      }
+    }
+  );
 });
 
 app.listen(8800, () => {
-	console.log("Server is running on port 8800");
-})
+  console.log("Server is running on port 8800");
+});
