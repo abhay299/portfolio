0d5135c15b3603640dfd673d31ed6b0eedf3dbbc
Abhay Gupta <abhaysg2000@gmail.com>
2023-07-11 22:13:54 +0530

    allow origin value change

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors({
 
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Private-Network", true)
-	res.header("Access-Control-Allow-Origin", true)
+	res.header("Access-Control-Allow-Origin")
 
 	next();
 })
