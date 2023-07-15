533eeabce025643a6371450984d619128426aed0
Abhay Gupta <abhaysg2000@gmail.com>
2023-07-16 04:38:56 +0530

    revamp Home layout design

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
-import Axios from 'axios';
+// import React, { useState } from 'react';
+// import Axios from 'axios';
 import './home.css';
-import smLogo from '../../assets/Socials/SM-RegisterPage.png';
-import baLogo from '../../assets/BookAmigo/BA-logo.png'
-import portfolioLogo from '../../assets/Portfolio/portfolioLogo.png'
-import crmLogo from '../../assets/CRM/CRM-homePage.png'
+import smLogo from '../../assets/Socials/SM-logo.png';
+import baLogo from '../../assets/BookAmigo/BA-logo2.png'
+import portfolioLogo from '../../assets/Portfolio/myPortfolioLogo.png'
+import crmLogo from '../../assets/CRM/CRM-logo.png'
 import me2 from '../../assets/Portfolio/me.JPG'
+import frontend from '../../assets/Portfolio/Frontend.png'
+import backend from '../../assets/Portfolio/Backend.png'
+import languageLogo from '../../assets/Portfolio/LanguagesLogo.png'
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 const Home = () => {
 	const navigate = useNavigate();
@@ -19,132 +23,395 @@ const Home = () => {
 		},
 	});
 
+	const onSubmit = (data) => {
+		// console.log(data)
+		reset();
+	}
 
+	const regEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 	// console.log(watch(register));
 
-	const [err, setErr] = useState(null);
+	// const [err, setErr] = useState(null);
 
-	const handleChange = (e) => {
-		setInputs(prev => ({
-			...prev, [e.target.name]: e.target.value
-		}));
-	};
+	// const handleChange = (e) => {
+	// 	setInputs(prev => ({
+	// 		...prev, [e.target.name]: e.target.value
+	// 	}));
+	// };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
+	// const handleSubmit = async (e) => {
+	// 	e.preventDefault();
 
-		if (err) { };
+	// 	if (err) { };
 
-		if (inputs.name === '' || inputs.email === '' || inputs.comment === '') {
-			return alert("Please fill out all fields.")
-		} else if (!regEmail.test(inputs.email)) {
-			return alert("Please enter a valid Email address.")
-		}
-		try {
-			await Axios.post("http://localhost:8800/", inputs);
-		} catch (err) {
-			setErr(err.response?.data)
-		}
+	// 	if (inputs.name === '' || inputs.email === '' || inputs.comment === '') {
+	// 		return alert("Please fill out all fields.")
+	// 	} else if (!regEmail.test(inputs.email)) {
+	// 		return alert("Please enter a valid Email address.")
+	// 	}
+	// 	try {
+	// 		await Axios.post("http://localhost:8800/", inputs);
+	// 	} catch (err) {
+	// 		setErr(err.response?.data)
+	// 	}
 
-		setInputs({
-			name: '',
-			email: '',
-			comment: ''
-		});
-		alert("Message sent!");
-	}
+	// 	setInputs({
+	// 		name: '',
+	// 		email: '',
+	// 		comment: ''
+	// 	});
+	// 	alert("Message sent!");
+	// }
 
 	return (
-		<div className='home'>
-			<div className='info'>
-				<p>Hey, I'm</p>
-				<h1>Abhay Gupta</h1>
-				<h2>Full Stack Web developer. </h2>
-				<p>Passionate about creating functional websites | Eager to contribute and grow professionally.</p>
+		<div className='Home'>
+			<div className='Hero'>
+				<div className='InfoContainer'>
+					<div className='InfoText'>
+						<p>Hey, I'm</p>
+						<h1>Abhay Gupta</h1>
+						<h2>Full Stack Web developer. </h2>
+						<p>Passionate about creating functional websites | Eager to contribute and grow professionally.</p>
+					</div>
+					{/* <div className='InfoImg'>
+					<img src={me2}
+						alt='myPhoto' />
+				</div> */}
+				</div>
+				<div className='Glowing'>
+					<span style={{ "--i": "1" }}></span>
+					<span style={{ "--i": "2" }}></span>
+					<span style={{ "--i": "3" }}></span>
+					<span style={{ "--i": "4" }}></span>
+				</div>
+				<div className='Glowing'>
+					<span style={{ "--i": "1" }}></span>
+					<span style={{ "--i": "2" }}></span>
+					<span style={{ "--i": "3" }}></span>
+					<span style={{ "--i": "4" }}></span>
+				</div>
+				<div className='Glowing'>
+					<span style={{ "--i": "1" }}></span>
+					<span style={{ "--i": "2" }}></span>
+					<span style={{ "--i": "3" }}></span>
+
+				</div>
+				<div className='Glowing'>
+					<span style={{ "--i": "1" }}></span>
+					<span style={{ "--i": "2" }}></span>
+					<span style={{ "--i": "3" }}></span>
+				</div>
 			</div>
-			<div className='work'>
+			{/* <div className='InfoContainer'>
+				<div className='InfoText'>
+					<p>Hey, I'm</p>
+					<h1>Abhay Gupta</h1>
+					<h2>Full Stack Web developer. </h2>
+					<p>Passionate about creating functional websites | Eager to contribute and grow professionally.</p>
+				</div>
+				<div className='InfoImg'>
+					<img src={me2}
+						alt='myPhoto' />
+				</div>
+			</div> */}
+			<div className='Work'>
 				<h1>
 					Over the months I've built a few projects...
 				</h1>
-				<div className='ProjectCardContainer'>
-					<img src={smLogo} alt='' onClick={() => {
-						navigate("/projects/0");
-					}} />
-					<div className='ContainerText'>
-						<h2>Socials</h2>
-						<p>
-							Full Stack Social Media App <br />
+				<div className='WorkContainer'>
+					<div className='ProjectCardContainer'>
+						<img src={smLogo} alt='' onClick={() => {
+							navigate("/projects/0");
+						}} />
+						<div className='Card-Content'>
+							<h2>Meet & Greet</h2>
+							<p>
+								Full Stack Social Media App <br />
+							</p>
 							<a href='/projects/0'>Find out more...</a>
-						</p>
+						</div>
 					</div>
-				</div>
-				<div className='ProjectCardContainer'>
-					<img src={baLogo} alt='' onClick={() => {
-						navigate("/projects/1");
-					}} />
-					<div className='ContainerText'>
-						<h2>BookAmigo</h2>
-						<p>
-							Front-End Hotel booking App <br />
+					<div className='ProjectCardContainer'>
+						<img src={baLogo} alt='' onClick={() => {
+							navigate("/projects/1");
+						}} />
+						<div className='Card-Content'>
+							<h2>Find Your Comfort Zone</h2>
+							<p>
+								Front-End Hotel booking App <br />
+							</p>
 							<a href='/projects/1'>Find out more...</a>
-						</p>
+						</div>
 					</div>
-				</div>
-				<div className='ProjectCardContainer'>
-					<img src={portfolioLogo} alt='' onClick={() => {
-						navigate("/projects/2");
-					}} />
-					<div className='ContainerText'>
-						<h2>My Portfolio</h2>
-						<p>
-							Full Stack Website <br />
+					<div className='ProjectCardContainer'>
+						<img src={portfolioLogo} alt='' onClick={() => {
+							navigate("/projects/2");
+						}} />
+						<div className='Card-Content'>
+							<h2>My Portfolio</h2>
+							<p>
+								Personal Website <br />
+							</p>
 							<a href='/' >Home</a>
-						</p>
+						</div>
+					</div>
+					<div className='ProjectCardContainer'>
+						<img src={crmLogo} alt='' onClick={() => {
+							navigate("/projects/3");
+						}} />
+						<div className='Card-Content'>
+							<h2>CRM System</h2>
+							<p>
+								Back-End based CRM App with Django <br />
+							</p>
+							<a href='/projects/3'>Find out more...</a>
+						</div>
 					</div>
 				</div>
-				<div className='ProjectCardContainer'>
-					<img src={crmLogo} alt='' onClick={() => {
-						navigate("/projects/3");
-					}} />
-					<div className='ContainerText'>
-						<h2>Django CRM</h2>
-						<p>
-							Back-End based CRM App with Django <br />
-							<a href='/projects/3'>Find out more...</a>
-						</p>
+			</div>
+			<div className='SkillHead'>
+				<h1>Tech Skills</h1>
+				<div className='Skill'>
+					<div className='SkillContainer'>
+						<div className='SkillCard'>
+							<div className='Face Face1'>
+								<div className='SkillContent'>
+									<div className='SkillIcon'>
+										<img className='fa'
+											aria-hidden='true'
+											src={frontend}
+											alt='frontend'></img>
+										{/* <h2>Frontend</h2> */}
+									</div>
+								</div>
+							</div>
+							<div className='Face Face2'>
+								<div className='Frontend'>
+									<div className='SkillContent'>
+										<p>
+											<a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+												<img
+													src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+													alt="react"
+													width="40"
+													height="40"
+												/>
+											</a>
+											<a href="https://redux.js.org" target="_blank" rel="noreferrer">
+												<img
+													src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"
+													alt="redux"
+													width="40"
+													height="40"
+												/>
+											</a>
+											<a href="https://getbootstrap.com" target="_blank" rel="noreferrer">
+												<img
+													src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
+													alt="bootstrap"
+													width="40"
+													height="40"
+												/>
+											</a>
+											<a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
+												<img
+													src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+													alt="html5"
+													width="40"
+													height="40"
+												/>
+											</a>
+											<a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
+												<img
+													src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+													alt="css3"
+													width="40"
+													height="40"
+												/>
+											</a>
+											<a href="https://sass-lang.com" target="_blank" rel="noreferrer">
+												<img
+													src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg"
+													alt="sass"
+													width="40"
+													height="40"
+												/>
+											</a>
+										</p>
+										<p>React, Redux, Bootstrap, HTML5, CSS3, SASS</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className='SkillCard'>
+							<div className='Face Face1'>
+								<div className='SkillContent'>
+									<div className='SkillIcon'>
+										<img className='fa'
+											aria-hidden='true'
+											src={backend}
+											alt='backend'></img>
+										{/* <h2>Backend</h2> */}
+									</div>
+								</div>
+							</div>
+							<div className='Face Face2'>
+								<div className='Backend'>
+									<div className='SkillContent'>
+										<p>
+											<a href="https://nodejs.org" target="_blank" rel="noreferrer">
+												<img
+													src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+													alt="nodejs"
+													width="40"
+													height="40"
+												/>
+											</a>
+											<a href="https://expressjs.com" target="_blank" rel="noreferrer">
+												<img
+													src="https://e7.pngegg.com/pngimages/925/447/png-clipart-express-js-node-js-javascript-mongodb-node-js-text-trademark.png"
+													alt="express"
+													width="40"
+													height="40"
+												/>
+											</a>
+											<a href="https://www.djangoproject.com/" target="_blank" rel="noreferrer">
+												<img
+													src="https://cdn.worldvectorlogo.com/logos/django.svg"
+													alt="django"
+													width="40"
+													height="40"
+												/>
+											</a>
+											<a href="https://www.mysql.com/" target="_blank" rel="noreferrer">
+												<img
+													src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
+													alt="mysql"
+													width="40"
+													height="40"
+												/>
+											</a>
+										</p>
+										<p>Node.js, Express.js, Django, MySQL,</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className='SkillCard'>
+							<div className='Face Face1'>
+								<div className='SkillContent'>
+									<div className='SkillIcon'>
+										<img className='fa'
+											aria-hidden='true'
+											src={languageLogo}
+											alt='languageLogo'></img>
+										{/* <h2>Languages</h2> */}
+									</div>
+								</div>
+							</div>
+							<div className='Face Face2'>
+								<div className='Language'>
+									<div className='SkillContent'>
+										<p>
+											<a
+												href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+												target="_blank" rel="noreferrer">
+												<img
+													src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+													alt="javascript"
+													width="40"
+													height="40"
+												/>
+											</a>
+											<a href="https://www.python.org" target="_blank" rel="noreferrer">
+												<img
+													src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
+													alt="python"
+													width="40"
+													height="40"
+												/>
+											</a>
+											<a href="https://www.java.com" target="_blank" rel="noreferrer">
+												<img
+													src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg"
+													alt="java"
+													width="40"
+													height="40"
+												/>
+											</a>
+										</p>
+										<p>JavaScript, Python, Java</p>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
+					{/* <h2>Tech Skills</h2> */}
+					{/* <ul className='SkillInfo'>
+					<li>
+						<img
+							src={frontend}
+							alt="frontend"
+							width="40"
+							height="40"
+						/>
+						Frontend:
+					</li>
+					<br />
+					<li>
+						<img
+							src={backend}
+							alt="backend"
+							width="40"
+							height="40"
+						/>
+						Backend:
+					</li>
+				</ul> */}
+					{/* <h2>Languages</h2> */}
+					{/* <ul className='SkillInfo'>
+					<li>
+						
+					</li>
+				</ul> */}
 				</div>
 			</div>
 			<div className='MailMeHeader'>
 				<h1>Drop me a message!</h1>
 				<p>Like my work or want to connect, just let me know.</p>
-				<form>
-					<input name='name' value={inputs.name} type="text" className='formInput'
-						placeholder='Enter your name' onChange={handleChange} required />
-					<input name='email' value={inputs.email} type="email" className='formInput'
-						placeholder='Enter your email' onChange={handleChange} required />
-					<textarea name='comment' value={inputs.comment} className='formInput' onChange={handleChange}
-						placeholder='Enter your message' required></textarea>
-					<button type='submit' className="sendButton"
-						onClick={handleSubmit}>Send</button>
-				</form>
-			</div>
-			<div className='skill'>
-				<h2>Tech Skills</h2>
-				<ul className='SkillInfo'>
-					<li>
-						Front-End: ReactJS, HTML, CSS
-					</li>
-					<br />
-					<li>
-						Back-End: NodeJS, ExpressJS, Django, MySQL
-					</li>
-				</ul>
-				<h2>Languages</h2>
-				<ul className='SkillInfo'>
-					<li>JavaScript, Python, Java, C</li>
-				</ul>
+				<div className='MailMe-InnerContent'>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<div className='Form-Row'>
+							{
+								<input className='Abhay FormInput' placeholder='Your name'
+									{...register('name', { required: true, maxLength: 60 })} />
+							}
+							{errors.name?.type === "required" && (
+								<p role="alert">Your name is required</p>
+							)}
+							<input className='Abhay FormInput' placeholder='Your email address'
+								{...register('email', { required: true, maxLength: 100, pattern: regEmail })} />
+							{errors.email && <p> Please fill out this field correctly.</p>}
+						</div>
+						<div className='Form-Row'>
+							<textarea className='FormInput' placeholder='Hi! We like some of your work, we would like to collaborate.'
+								{...register('comment', { required: true, })} />
+							{errors.comment && <p> Please fill out this field.</p>}
+						</div>
+						{/* <input className='SendButton' type='submit' /> */}
+						<div className='BtnBody' type='submit'>
+							<div id='BtnContainer'>
+								<button className='Learn-More'>
+									<span className='Circle' aria-hidden="true">
+										<span className='Icon Arrow'></span>
+									</span>
+									<span className='Button-Text'>SEND</span>
+								</button>
+							</div>
+						</div>
+					</form>
+				</div>
 			</div>
 		</div>
 	)
