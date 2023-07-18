cf216b055ef0d1d7d8bada705e39e7654abd4947
Abhay Gupta <abhaysg2000@gmail.com>
2023-07-18 08:04:01 +0530

    changed scroll behaviour for project page

--- src/pages/projectInfo/ProjectInfo.jsx
+++ src/pages/projectInfo/ProjectInfo.jsx
@@ -21,33 +21,17 @@ const ProjectInfo = () => {
 	};
 
 	return (
-		<div className='Project'>
-			<h1>{project.name}</h1>
-			<p>Stack: {project.stack}</p>
-			<div className='TextContainer'>
-				<p>{project.desc}</p>
-			</div>
-			<div className='CarouselContainer'>
+		<section>
+			<div className='Project'>
+				<h1>{project.name}</h1>
+				<p>Stack: {project.stack}</p>
+				<div className='TextContainer'>
+					<p>{project.desc}</p>
+				</div>
+				<div className='CarouselContainer'>
 
-				{
-					(project.imgLen === 2) ?
-						<Carousel data-bs-theme="dark">
-							<Carousel.Item>
-								<img
-									className='d-block w-100'
-									src={project?.image}
-									alt='First slide'
-								/>
-							</Carousel.Item>
-							<Carousel.Item>
-								<img
-									className='d-block w-100'
-									src={project?.image1}
-									alt='Second slide'
-								/>
-							</Carousel.Item>
-						</Carousel>
-						: (project.imgLen === 4) ?
+					{
+						(project.imgLen === 2) ?
 							<Carousel data-bs-theme="dark">
 								<Carousel.Item>
 									<img
@@ -63,78 +47,96 @@ const ProjectInfo = () => {
 										alt='Second slide'
 									/>
 								</Carousel.Item>
-								<Carousel.Item>
-									<img
-										className='d-block w-100'
-										src={project?.image2}
-										alt='Third slide'
-									/>
-
-								</Carousel.Item>
-								<Carousel.Item>
-									<img
-										className='d-block w-100'
-										src={project?.image3}
-										alt='Fourth slide'
-									/>
-								</Carousel.Item>
 							</Carousel>
-							:
-							<Carousel data-bs-theme="dark">
-								<Carousel.Item>
-									<img
-										className='d-block w-100'
-										src={project?.image}
-										alt='First slide'
-									/>
-								</Carousel.Item>
-								<Carousel.Item>
-									<img
-										className='d-block w-100'
-										src={project?.image1}
-										alt='Second slide'
-									/>
-								</Carousel.Item>
-								<Carousel.Item>
-									<img
-										className='d-block w-100'
-										src={project?.image2}
-										alt='Third slide'
-									/>
+							: (project.imgLen === 4) ?
+								<Carousel data-bs-theme="dark">
+									<Carousel.Item>
+										<img
+											className='d-block w-100'
+											src={project?.image}
+											alt='First slide'
+										/>
+									</Carousel.Item>
+									<Carousel.Item>
+										<img
+											className='d-block w-100'
+											src={project?.image1}
+											alt='Second slide'
+										/>
+									</Carousel.Item>
+									<Carousel.Item>
+										<img
+											className='d-block w-100'
+											src={project?.image2}
+											alt='Third slide'
+										/>
 
-								</Carousel.Item>
-								<Carousel.Item>
-									<img
-										className='d-block w-100'
-										src={project?.image3}
-										alt='Fourth slide'
-									/>
-								</Carousel.Item>
-								<Carousel.Item>
-									<img
-										className='d-block w-100'
-										src={project?.image4}
-										alt='Fifth slide'
-									/>
-								</Carousel.Item>
-								<Carousel.Item>
-									<img
-										className='d-block w-100'
-										src={project?.image5}
-										alt='Sixth slide'
-									/>
-								</Carousel.Item>
-							</Carousel>
-				}
+									</Carousel.Item>
+									<Carousel.Item>
+										<img
+											className='d-block w-100'
+											src={project?.image3}
+											alt='Fourth slide'
+										/>
+									</Carousel.Item>
+								</Carousel>
+								:
+								<Carousel data-bs-theme="dark">
+									<Carousel.Item>
+										<img
+											className='d-block w-100'
+											src={project?.image}
+											alt='First slide'
+										/>
+									</Carousel.Item>
+									<Carousel.Item>
+										<img
+											className='d-block w-100'
+											src={project?.image1}
+											alt='Second slide'
+										/>
+									</Carousel.Item>
+									<Carousel.Item>
+										<img
+											className='d-block w-100'
+											src={project?.image2}
+											alt='Third slide'
+										/>
+
+									</Carousel.Item>
+									<Carousel.Item>
+										<img
+											className='d-block w-100'
+											src={project?.image3}
+											alt='Fourth slide'
+										/>
+									</Carousel.Item>
+									<Carousel.Item>
+										<img
+											className='d-block w-100'
+											src={project?.image4}
+											alt='Fifth slide'
+										/>
+									</Carousel.Item>
+									<Carousel.Item>
+										<img
+											className='d-block w-100'
+											src={project?.image5}
+											alt='Sixth slide'
+										/>
+									</Carousel.Item>
+								</Carousel>
+					}
+				</div>
+				<div className='ProjectFooter'>
+					<button className='PrevBtn' disabled={parseInt(id) === 0 ? true : false} onClick={handlePrev}>prev</button>
+					<a href={project.gitUrl}>
+						<GitHub fontSize='large' />
+					</a>
+					<button className='NextBtn' disabled={parseInt(id) === projectList.length - 1 ? true : false} onClick={handleNext} >next</button>
+				</div>
 			</div>
-			<div className='ProjectFooter'>
-				<button className='PrevBtn' disabled={parseInt(id) === 0 ? true : false} onClick={handlePrev}>prev</button>
-				<a href={project.gitUrl}>
-					<GitHub fontSize='large' />
-				</a>
-				<button className='NextBtn' disabled={parseInt(id) === projectList.length - 1 ? true : false} onClick={handleNext} >next</button>
-			</div>
-		</div>
+		</section>
 	)
 };
 
