de3164553fdef291f7d6c6669c92e123dd9e5290
abhay299 <abhaysg2000@gmial.com>
2025-04-18 15:56:43 +0530

    updated the work component

--- src/pages/home/components/Work/index.tsx
+++ src/pages/home/components/Work/index.tsx
@@ -1,37 +1,58 @@
 import { GitHub } from "@mui/icons-material";
 import React from "react";
 
+import AuroraBackground from "../../../../components/AuroraBackground";
+import TiltedCard from "../../../../components/TiltedCard";
 import { projectCards } from "../../../../helpers/projectCards";
+
+import styles from "./index.module.css";
 
 function Work({ navigate }) {
 	return (
-		<div className="Work">
-			<h1>Over the months I've built a few projects...</h1>
-			<div className="WorkContainer">
+		<div className={styles.work}>
+			<AuroraBackground
+				colorStops={["#000000", "#000000", "#000000"]}
+				blend={0.2}
+				amplitude={0.8}
+				speed={0.6}
+			/>
+			<h1 className={styles.work_heading}>
+				Over the months I've built a few projects...
+			</h1>
+
+			<div className={styles.work_container}>
 				{projectCards.map((project, index) => (
-					<div className="ProjectCardContainer" key={index}>
-						<img
-							src={project.image}
-							alt="Project-Logo"
-							onClick={() => {
-								navigate(`/projects/${index}`);
-							}}
+					<div key={index} className={styles.project_card_container}>
+						<TiltedCard
+							imageSrc={project.image}
+							altText={project.title}
+							containerHeight="36rem"
+							containerWidth="100%"
+							imageHeight="100%"
+							imageWidth="100%"
+							rotateAmplitude={12}
+							scaleOnHover={1.1}
+							showMobileWarning={false}
+							showTooltip={false}
+							displayOverlayContent={true}
+							onImageClick={() => navigate(`/projects/${index}`)}
+							overlayContent={
+								<div className={styles.project_card}>
+									<div className={styles.project_card_text}>
+										<h2>{project.title}</h2>
+										{/* <p>{project.desc}</p> */}
+										<span>
+											<a href={`/projects/${index}`}>
+												Find out more...
+											</a>
+											<a href={project.gitUrl}>
+												<GitHub />
+											</a>
+										</span>
+									</div>
+								</div>
+							}
 						/>
-						<div>
-							<h2>{project.title}</h2>
-							<p>
-								{project.desc}
-								<br />
-							</p>
-							<span>
-								<a href={`/projects/${index}`}>
-									Find out more...
-								</a>
-								<a href={project.gitUrl}>
-									<GitHub />
-								</a>
-							</span>
-						</div>
 					</div>
 				))}
 			</div>
