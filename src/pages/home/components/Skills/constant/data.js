928f615eb181289f0bf98b1d451cd37135677691
abhay299 <abhaysg2000@gmial.com>
2025-04-18 19:42:45 +0530

    added mapping of skills data

---
+++ src/pages/home/components/Skills/constant/data.js
@@ -0,0 +1,18 @@
+export const frontend = [
+	{ name: "ReactJs", icon: "/icons/react-svgrepo.svg" },
+	{ name: "Sass", icon: "/icons/sass-svgrepo.svg" },
+	{ name: "CSS3", icon: "/icons/css-3-svgrepo.svg" },
+	// { name: "Tailwind", icon: "/icons/tailwind-svgrepo.svg" },
+];
+
+export const backend = [
+	{ name: "NodeJs", icon: "/icons/nodejs-logo-svgrepo.svg" },
+	{ name: "MySQL", icon: "/icons/mysql-logo-svgrepo.svg" },
+	{ name: "MongoDB", icon: "/icons/mongodb-svgrepo.svg" },
+];
+
+export const languages = [
+	{ name: "JavaScript", icon: "/icons/javascript-svgrepo.svg" },
+	{ name: "TypeScript", icon: "/icons/typescript-svgrepo.svg" },
+	{ name: "Python", icon: "/icons/python-svgrepo.svg" },
+];
