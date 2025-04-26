d37b0add9f5697a828e7317b63c15e2ae47f88ad
abhay299 <abhaysg2000@gmial.com>
2025-04-26 12:36:22 +0530

    refactor: mail component refactored

---
+++ src/pages/home/components/Mail/index.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+import { regEmail } from "../../../../constants";
+
+import styles from "./index.module.css";
+
+const Mail = ({ handleSubmit, register, errors, onSubmit }) => {
+	return (
+		<div className={styles.mail_me_header}>
+			<h1>Drop me a message!</h1>
+			<p>Like my work or want to connect, just let me know.</p>
+
+			<div className={styles.mail_me_inner_content}>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<div className={styles.form_row}>
+						<input
+							className={`${styles.abhay} ${styles.form_input}`}
+							placeholder="Your name."
+							{...register("name", {
+								required: true,
+								maxLength: 60,
+							})}
+						/>
+						{errors.name?.type === "required" && (
+							<p role="alert">Your name is required.</p>
+						)}
+						<input
+							className={`${styles.abhay} ${styles.form_input}`}
+							placeholder="Your email address."
+							{...register("email", {
+								required: true,
+								maxLength: 100,
+								pattern: regEmail,
+							})}
+						/>
+						{errors.email && (
+							<p>Please fill out this field correctly.</p>
+						)}
+					</div>
+
+					<div className={styles.form_row}>
+						<textarea
+							className={styles.form_input}
+							placeholder="Hello, we would like to discuss about so and so position/project with you."
+							{...register("comment", { required: true })}
+						/>
+						{errors.comment && <p>Please fill out this field.</p>}
+					</div>
+
+					<div className={styles.btn_body}>
+						<div id="BtnContainer">
+							<button className={styles.learn_more} type="submit">
+								<span
+									className={styles.circle}
+									aria-hidden="true"
+								>
+									<span
+										className={`${styles.icon} ${styles.arrow}`}
+									/>
+								</span>
+								<span className={styles.button_text}>SEND</span>
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default Mail;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,7 @@
 import "./home.css";
 
-import { regEmail } from "../../constants/index.js";
-
 import Hero from "./components/Hero/index.tsx";
+import Mail from "./components/Mail/index.tsx";
 import Skills from "./components/Skills/index.tsx";
 import Work from "./components/Work/index.tsx";
 import useHomePage from "./hooks/useHomePage.js";
@@ -16,63 +15,12 @@ function Home() {
 			<Hero />
 			<Work navigate={navigate} />
 			<Skills />
-
-			<div className="MailMeHeader">
-				<h1>Drop me a message!</h1>
-				<p>Like my work or want to connect, just let me know.</p>
-				<div className="MailMe-InnerContent">
-					<form onSubmit={handleSubmit(onSubmit)}>
-						<div className="Form-Row">
-							{
-								<input
-									className="Abhay FormInput"
-									placeholder="Your name."
-									{...register("name", {
-										required: true,
-										maxLength: 60,
-									})}
-								/>
-							}
-							{errors.name?.type === "required" && (
-								<p role="alert">Your name is required.</p>
-							)}
-							<input
-								className="Abhay FormInput"
-								placeholder="Your email address."
-								{...register("email", {
-									required: true,
-									maxLength: 100,
-									pattern: regEmail,
-								})}
-							/>
-							{errors.email && (
-								<p> Please fill out this field correctly.</p>
-							)}
-						</div>
-						<div className="Form-Row">
-							<textarea
-								className="FormInput"
-								placeholder="Hello, we would like to discuss about so and so position/project with you."
-								{...register("comment", { required: true })}
-							/>
-							{errors.comment && (
-								<p> Please fill out this field.</p>
-							)}
-						</div>
-
-						<div className="BtnBody" type="submit">
-							<div id="BtnContainer">
-								<button className="Learn-More">
-									<span className="Circle" aria-hidden="true">
-										<span className="Icon Arrow" />
-									</span>
-									<span className="Button-Text">SEND</span>
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
+			<Mail
+				handleSubmit={handleSubmit}
+				register={register}
+				errors={errors}
+				onSubmit={onSubmit}
+			/>
 		</div>
 	);
 }
