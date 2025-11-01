30df1452482014b109780ab5efe86ba145722bf4
abhay299 <abhaysg2000@gmial.com>
2025-11-01 15:49:27 +0530

    refactor: navigationLinks and email regex constants

---
+++ src/constants/navigationLinks.js
@@ -0,0 +1,18 @@
+const NAVIGATION_LINKS = [
+	{ path: "/", label: "Home" },
+	{ path: "/projects", label: "Work" },
+	{ path: "/aboutMe", label: "About" },
+	{
+		path: "https://drive.google.com/file/d/1f8NoJjYrAB-oKEv_yuEx5n6gcn1iqjRa/view?usp=sharing",
+		label: "Resume",
+		external: true,
+	},
+	{
+		path: "/#contact",
+		label: "Contact",
+		external: false,
+		isContact: true,
+	},
+];
+
+export { NAVIGATION_LINKS };
--- src/constants/regex.js
+++ src/constants/regex.js
@@ -1,2 +1,4 @@
-export const regEmail =
+const EMAIL_REGEX =
 	/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+export { EMAIL_REGEX };
